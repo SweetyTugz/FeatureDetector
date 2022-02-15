8e6f3da0a3bf82cb7595f83642f5154765661279
Sweety Tugnawat <st20acv@herts.ac.uk>
2022-02-15 12:52:30 +0000

    Added the matcher to match the descriptors together

--- FeatureDetector.py
+++ FeatureDetector.py
@@ -18,10 +18,25 @@ print(des1.shape)
 #cv2.imshow('KP1',imgKp1)
 #cv2.imshow('KP2',imgKp2)
 
+#will return the match
+bf= cv2.BFMatcher()
+matches=bf.knnMatch(des1,des2,k=2)
+
+#0.75 is good approximation
+good=[]
+for m,n in matches:
+    if m.distance<0.75 *n.distance:
+        good.append([m])
+
+#number of matches we are getting
+print(len(good))
+
+img3=cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
 
 
 cv2.imshow('img1',img1)
 cv2.imshow('img2',img2)
+cv2.imshow('img3',img3)
 cv2.waitKey(0)
 
 
